b9ac25d9ae345c003946cc864ea735ca3e9195d9
Prabhat Rawat <prabhat.rawat@nagarro.com>
2023-04-15 17:45:07 +0530

    Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA],
+
   imports: [
     BrowserModule
   ],
